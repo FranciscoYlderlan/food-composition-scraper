8fa0601e118b05d1aae8b3c69739e859a9c4fba7
franciscoylderlan <ylderlan@nca.ufma.br>
2025-02-02 23:32:51 -0300

    :sparkles: feat/backend: Create ComponentService

---
+++ backend/src/Services/ComponentService.cs
@@ -0,0 +1,28 @@
+﻿using backend.Data;
+using backend.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Services
+{
+    public interface IComponentService
+    {
+        Task<IEnumerable<Component>> GetComponentsByFoodItemCodeAsync(string foodItemCode);
+    }
+
+    public class ComponentService : IComponentService
+    {
+        private readonly AppDbContext _context;
+
+        public ComponentService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Component>> GetComponentsByFoodItemCodeAsync(string foodItemCode)
+        {
+            return await _context.Components
+                .Where(c => c.FoodItemCode == foodItemCode)
+                .ToListAsync();
+        }
+    }
+}
