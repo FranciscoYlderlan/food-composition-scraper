bf5604af383f52deb882bc23dd97d37cadf386ae
franciscoylderlan <ylderlan@nca.ufma.br>
2025-02-03 10:05:37 -0300

    :sparkles: feat/frontend: Establish fetch for getComponents endpoint

--- frontend/src/app/(home)/details/[code]/page.tsx
+++ frontend/src/app/(home)/details/[code]/page.tsx
@@ -1,8 +1,29 @@
+import { ComponentSchemaType } from '@/models/schemas/zod/component-schema'
+import { Suspense } from 'react'
+import { ComponentTable } from '../component-table'
+
+async function getComponents(code: string): Promise<ComponentSchemaType[]> {
+  const response = await fetch(
+    `http://localhost:5172/api/FoodItem/${code}/components
+`,
+    { cache: 'no-store' }
+  )
+  return response.json()
+}
+
 export default async function Component({
   params,
 }: {
   params: Promise<{ code: string }>
 }) {
   const { code } = await params
-  return <div>É isso aiiiiiii {code}</div>
+  const components = await getComponents(code)
+
+  return (
+    <div>
+      <Suspense fallback={<>Carregando ...</>}>
+        <ComponentTable data={components} />
+      </Suspense>
+    </div>
+  )
 }
--- frontend/src/app/(home)/details/component-table.tsx
+++ frontend/src/app/(home)/details/component-table.tsx
@@ -14,29 +14,24 @@ import {
   getCoreRowModel,
   useReactTable,
 } from '@tanstack/react-table'
-import { useRouter } from 'next/navigation'
 
 type ComponentTableProps = {
   data: ComponentSchemaType[]
 }
 
 export function ComponentTable({ data }: ComponentTableProps) {
-  const router = useRouter()
   const columnHeadersArray: Array<keyof ComponentSchemaType> = [
-    'id',
     'name',
     'unit',
-    'foodItemCode',
-    'name',
+    'standardDeviation',
+    'minValue',
+    'maxValue',
     'dataCount',
+    'references',
+    'dataType',
   ]
-  const columnHelper = createColumnHelper<ComponentSchemaType>()
 
-  // const columns = [
-  //   columnHelper.accessor('code', {
-  //     header: 'Código',
-  //   }),
-  // ]
+  const columnHelper = createColumnHelper<ComponentSchemaType>()
 
   const columns = columnHeadersArray.map((columnName) => {
     return columnHelper.accessor(columnName, {
@@ -56,28 +51,32 @@ export function ComponentTable({ data }: ComponentTableProps) {
       <div className="mt-6 overflow-hidden rounded-lg border border-border">
         <Table className="border">
           <TableHeader>
-            {table.getHeaderGroups().map(({ id, depth, headers }) => (
-              <TableRow key={id}>
-                {headers.map(({ id, column, isPlaceholder, getContext }) => (
-                  <TableHead key={id} className="bg-secondary">
-                    <div>
-                      {isPlaceholder
-                        ? null
-                        : flexRender(column.columnDef.header, getContext())}
-                    </div>
-                  </TableHead>
-                ))}
+            {table.getHeaderGroups().map(({ id, headers }, index) => (
+              <TableRow key={id + index}>
+                {headers.map(
+                  ({ id, column, isPlaceholder, getContext }, index) => (
+                    <TableHead key={id + index} className="bg-secondary">
+                      <div>
+                        {isPlaceholder
+                          ? null
+                          : flexRender(column.columnDef.header, getContext())}
+                      </div>
+                    </TableHead>
+                  )
+                )}
               </TableRow>
             ))}
           </TableHeader>
           <TableBody>
-            {table.getRowModel().rows.map((row) => (
-              <TableRow key={row.id}>
-                {row.getVisibleCells().map(({ id, column, getContext }) => (
-                  <TableCell key={id} className="border">
-                    {flexRender(column.columnDef.cell, getContext())}
-                  </TableCell>
-                ))}
+            {table.getRowModel().rows.map((row, index) => (
+              <TableRow key={row.id + index}>
+                {row
+                  .getVisibleCells()
+                  .map(({ id, column, getContext }, index) => (
+                    <TableCell key={id + index} className="border">
+                      {flexRender(column.columnDef.cell, getContext())}
+                    </TableCell>
+                  ))}
               </TableRow>
             ))}
           </TableBody>
