7af76b21542251e45c80a832479eefc0b1129d2e
franciscoylderlan <ylderlan@nca.ufma.br>
2025-02-02 21:36:21 -0300

    :package: chore/backend: Add migrations

---
+++ backend/src/Migrations/20250202225037_Initial migration.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FoodItems",
+                columns: table => new
+                {
+                    Code = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    ScientificName = table.Column<string>(type: "text", nullable: true),
+                    Group = table.Column<string>(type: "text", nullable: false),
+                    Brand = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodItems", x => x.Code);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Components",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Unit = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<double>(type: "double precision", nullable: true),
+                    StandardDeviation = table.Column<double>(type: "double precision", nullable: true),
+                    MinValue = table.Column<double>(type: "double precision", nullable: true),
+                    MaxValue = table.Column<double>(type: "double precision", nullable: true),
+                    DataCount = table.Column<int>(type: "integer", nullable: true),
+                    References = table.Column<string>(type: "text", nullable: true),
+                    DataType = table.Column<string>(type: "text", nullable: true),
+                    FoodItemCode = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Components", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Components_FoodItems_FoodItemCode",
+                        column: x => x.FoodItemCode,
+                        principalTable: "FoodItems",
+                        principalColumn: "Code",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Components_FoodItemCode",
+                table: "Components",
+                column: "FoodItemCode");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Components");
+
+            migrationBuilder.DropTable(
+                name: "FoodItems");
+        }
+    }
+}
