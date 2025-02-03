98d863bbc000d45f9cbe3a400f94a89d8dd3d19f
franciscoylderlan <ylderlan@nca.ufma.br>
2025-02-03 03:03:44 -0300

    :wrench: chore/backend: Configure CORS and add local environment variables

--- backend/src/Program.cs
+++ backend/src/Program.cs
@@ -4,13 +4,24 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Adicionando o serviço de log
+builder.Services.AddLogging();
+
+// Adiciona os serviços da aplicação
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Conexão com o banco de dados, agora usando as variáveis de ambiente
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection")));
+{
+    var dbConnection = builder.Configuration["DbConnection"]; // Usando a variável de ambiente DbConnection
+    if (string.IsNullOrEmpty(dbConnection))
+    {
+        throw new InvalidOperationException("A string de conexão do banco de dados não foi configurada.");
+    }
+    options.UseNpgsql(dbConnection);
+});
 
 builder.Services.AddHttpClient<IWebScrapingService, WebScrapingService>();
 builder.Services.AddScoped<IFoodItemService, FoodItemService>();
@@ -27,16 +38,22 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 // Chama o Seeder assim que a aplicação for iniciada
 using (var scope = app.Services.CreateScope())
 {
     var seeder = scope.ServiceProvider.GetRequiredService<DatabaseSeeder>();
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<DatabaseSeeder>>();  // Obtém o Logger
+
+    // Logando quando o Seeder for iniciado
+    logger.LogInformation("Iniciando o processo de Seed no banco de dados...");
+
     await seeder.SeedAsync();
+
+    // Logando quando o Seeder for concluído
+    logger.LogInformation("Processo de Seed concluído.");
 }
 
 app.Run();
