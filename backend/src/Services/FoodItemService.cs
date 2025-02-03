c4f66aa2227d45227036f11ab6a5ac37e14f374b
franciscoylderlan <ylderlan@nca.ufma.br>
2025-02-02 23:31:05 -0300

    :sparkles: feat/backend: Create FoodItemService

---
+++ backend/src/Services/FoodItemService.cs
@@ -0,0 +1,36 @@
+﻿using backend.Data;
+using backend.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Services
+{
+    public interface IFoodItemService
+    {
+        Task<IEnumerable<FoodItem>> GetFoodItemsAsync(string search, int page, int pageSize);
+    }
+
+    public class FoodItemService : IFoodItemService
+    {
+        private readonly AppDbContext _context;
+
+        public FoodItemService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<FoodItem>> GetFoodItemsAsync(string search, int page, int pageSize)
+        {
+            var query = _context.FoodItems.AsQueryable();
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                query = query.Where(f => f.Name.Contains(search));
+            }
+
+            return await query
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+    }
+}
