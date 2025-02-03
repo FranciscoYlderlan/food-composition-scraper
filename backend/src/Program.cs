a5577e5398fa7faa6b791f97ff04814daa996125
franciscoylderlan <ylderlan@nca.ufma.br>
2025-02-03 00:09:27 -0300

    :sparkles: feat/backend: Add HTTP request routes

--- backend/src/Program.cs
+++ backend/src/Program.cs
@@ -1,4 +1,5 @@
 using backend.Data;
+using backend.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options => {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection"));
 });
+
+builder.Services.AddHttpClient<IWebScrapingService, WebScrapingService>();
+builder.Services.AddScoped<IFoodItemService, FoodItemService>();
+builder.Services.AddScoped<IComponentService, ComponentService>();
 
 var app = builder.Build();
 
