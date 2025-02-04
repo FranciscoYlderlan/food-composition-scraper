a43594be98e601b770599d204c3339d0f2df187f
franciscoylderlan <ylderlan@nca.ufma.br>
2025-02-04 13:03:23 -0300

    :zap: refact/backend(implement-tests): Add dto pagination type  GetFoodItems return

---
+++ backend/src/Dtos/Pagination.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.Dtos
+{
+    public class Pagination
+    {
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+    }
+}
+
